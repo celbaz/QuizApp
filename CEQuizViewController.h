b185f3934b954836be16e8d56ddb038ec4f8deca
Carl Elbaz <carl.elbaz@gmail.com>
2015-06-17 17:26:42 -0400

    First iOS Project!

---
+++ CEQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  CEQuizViewController.h
+//  Quiz
+//
+//  Created by Carl Elbaz on 6/16/15.
+//  Copyright (c) 2015 Carl Elbaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CEQuizViewController : UIViewController
+
+@end
--- Quiz/AppDelegate.m
+++ Quiz/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "CEQuizViewController.h"
 
 @interface AppDelegate ()
 
@@ -14,14 +15,20 @@
 
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    // Override point for customization after application launch
+    
+    CEQuizViewController *quizVC = [[CEQuizViewController alloc] init];
+    self.window.rootViewController = quizVC;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
